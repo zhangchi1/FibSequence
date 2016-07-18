9add0e53b67b4be5caad6ff0d53dc87059d99dbc
chi zhang <zhang.chi1@husky.neu.edu>
2016-07-18 15:29:33 -0400

    Fib Sequence

---
+++ fib sequence.R
@@ -0,0 +1,26 @@
+fibslow  <- function(n)
+{
+  if (length(n) > 1) return(sapply(n, fib)) # accept a numeric vector
+  if (n == 1) return(1) # first seed value
+  if (n == 2) return(1) # second seed value
+  return(fib(n-1)+fib(n-2)) # use recursion
+}
+
+# print first five Fibonacci numbers
+fibslow(1)
+fibslow(2)
+fibslow(3)
+fibslow(4)
+fibslow(5)
+
+#quicker fib
+fibk <- function(n) {
+  len = 400
+  fibvals = numeric(len)
+  fibvals[1] = 1
+  fibvals[2] = 1
+  for (i in 3:n) {
+    fibvals[i] = fibvals[i-1] + fibvals[i-2]
+  }
+  return (fibvals[n])
+}
